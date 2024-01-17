30185401c381c46faba6858a1a9ab51ffb005deb
dinesh-saini814 <dm43102154@gmail.com>
2024-01-17 15:39:06 +0530

    basic js for print input value on screen is complete

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const buttonEle = document.querySelector("button");
 const inputEle = document.querySelector("input");
 
+const ulEl = document.querySelector("#ul-el");
+
+let store_url = [];
+
 buttonEle.addEventListener("click", () => {
-  const value = inputEle.value;
-  alert(value);
+  store_url.push(inputEle.value);
+  console.log(store_url);
+
+  //   ulEl.innerHTML += "<li>" + inputEle.value + "</li>";
+  for (let i = 0; i < store_url.length; i++) {
+    ulEl.innerHTML += "<li>" + store_url[i] + "</li>";
+  }
+  inputEle.value = "";
+  store_url = [];
 });
