7b37fb2c91964ff01059ed52b010b0e4f398a0ab
dinesh-saini814 <dm43102154@gmail.com>
2024-01-17 21:11:25 +0530

    url and text issue solved

--- app.js
+++ app.js
@@ -3,21 +3,48 @@ const inputEle = document.querySelector(".input_element");
 const inputext = document.querySelector(".input_text");
 
 const ulEl = document.querySelector("#ul-el");
+const ulEl2 = document.querySelector("#ul-el2");
+
+const link_div = document.querySelector(".link_div");
 
 let store_url = [];
+let store_text = [];
 
 buttonEle.addEventListener("click", () => {
   store_url.push(inputEle.value);
-  render();
+  store_text.push(inputext.value);
+
   inputEle.value = "";
+  inputext.value = "";
+
+  render_div();
 });
 
-function render() {
+// function render() {
+//   let listitems = "";
+//   let listtag = "";
+
+//   for (let i = 0; i < store_url.length; i++) {
+//     listitems += `<li><a target='_blank' href='${store_url[i]}'>${store_url[i]}</a></li>`;
+//     listtag += `<li>${store_text[i]}</li>`;
+//   }
+
+//   ulEl.innerHTML = listitems;
+//   ulEl2.innerHTML = listtag;
+// }
+
+function render_div() {
+  store_url.push(inputEle.value);
+  store_text.push(inputext.value);
+
   let listitems = "";
 
-  for (let i = 0; i < store_url.length; i++) {
-    listitems += `<li><a target='_blank' href='${store_url[i]}'>${store_url[i]}</a></li>`;
-  }
+  console.log(store_text);
+  console.log(store_text);
 
-  ulEl.innerHTML = listitems;
+  for (let i = 0; i < store_url.length; i++) {
+    link_div.innerHTML += `<div class='textdisplay'>${store_text[i]}</br><a target='_blank' href='${store_url[i]}'>${store_url[i]}</a></div>`;
+    store_url = [];
+    store_text = [];
+  }
 }
