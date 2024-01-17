ac15058ecea294c91d91cbf819746e22ef152a5a
dinesh-saini814 <dm43102154@gmail.com>
2024-01-18 00:34:57 +0530

    solve empty input filf error

--- app.js
+++ app.js
@@ -2,42 +2,36 @@ const buttonEle = document.querySelector("button");
 const inputEle = document.querySelector(".input_element");
 const inputext = document.querySelector(".input_text");
 
-// const ulEl = document.querySelector("#ul-el");
-// const ulEl2 = document.querySelector("#ul-el2");
-
 const link_div = document.querySelector(".link_div");
 
 let store_url = [];
 let store_text = [];
 
 buttonEle.addEventListener("click", () => {
-  store_url.push(inputEle.value);
-  store_text.push(inputext.value);
+  if (inputEle.value === "" || inputext.value === "") {
+    changeColor_place("red");
 
-  inputEle.value = "";
-  inputext.value = "";
+    return;
+  } else {
+    store_url.push(inputEle.value);
+    store_text.push(inputext.value);
 
-  render_div();
+    inputEle.value = "";
+    inputext.value = "";
+
+    render_div();
+    colourChanger();
+    changeColor_place("rgb(88, 88, 88)");
+  }
+
+  if (inputEle.value.includes("https://") === false) {
+    inputEle.value = "https://" + inputEle.value;
+  }
 });
-
-// function render() {
-//   let listitems = "";
-//   let listtag = "";
-
-//   for (let i = 0; i < store_url.length; i++) {
-//     listitems += `<li><a target='_blank' href='${store_url[i]}'>${store_url[i]}</a></li>`;
-//     listtag += `<li>${store_text[i]}</li>`;
-//   }
-
-//   ulEl.innerHTML = listitems;
-//   ulEl2.innerHTML = listtag;
-// }
 
 function render_div() {
   store_url.push(inputEle.value);
   store_text.push(inputext.value);
-
-  let listitems = "";
 
   console.log(store_text);
   console.log(store_text);
@@ -49,10 +43,18 @@ function render_div() {
   }
 }
 
-var colorBoxes = document.querySelectorAll(".color-box");
-for (var i = 0; i < colorBoxes.length; i++) {
-  colorBoxes[i].addEventListener("click", function () {
-    var color = this.getAttribute("data-color");
-    document.querySelector(".textdisplay").style.backgroundColor = color;
-  });
+function colourChanger() {
+  var colorBoxes = document.querySelectorAll(".color-box");
+  for (var i = 0; i < colorBoxes.length; i++) {
+    colorBoxes[i].addEventListener("click", function () {
+      var color = this.getAttribute("data-color");
+      document.querySelector(`.`).style.backgroundColor = color;
+    });
+  }
+}
+
+function changeColor_place(toColor) {
+  addCSS = document.createElement("style");
+  addCSS.innerHTML = "::placeholder { color: " + toColor + "; }";
+  document.body.append(addCSS);
 }
